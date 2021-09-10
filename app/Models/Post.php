86382d726e162205cb33d17adf523616a762b215
ZoeyF75 <zf.zoodle@gmail.com>
2021-09-10 14:57:46 -0700

    dynamic posts/homepage route

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File; //static access to all sorts of functionality
 
 class Post
 {
+  public static function all()
+  {
+    $files = File::files(resource_path("posts/"));
+
+    return array_map(fn($file) => $file->getContents(), $files);
+  }
+
   public static function find($slug)
   {
     if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' =>  $posts //how to get those posts...
+        'posts' => Post::all()
     ]);
 });
 
