51713c8296af3b87da879c1e1731eff6a8af3b79
ZoeyF75 <zf.zoodle@gmail.com>
2021-09-13 11:37:11 -0700

    using collections to display posts

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,15 @@ use Symfony\Component\Yaml\Yaml;
 */
 
 Route::get('/', function () {
-    $files = File::files(resource_path("posts"));
-
-    $posts = array_map(function ($file) {
-        $document = YamlFrontMatter::parseFile($file);
-        return new Post(
+    $posts = collect(File::files(resource_path("posts")))
+        ->map(fn($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn ($document) => new Post(
             $document->title,
             $document->excerpt,
             $document->date,
             $document->body(),
             $document->slug
-        );
-    }, $files);
+        ));
 
     return view('posts', [
         'posts' => $posts
